e1526ab7989fff719b1a186c02bcc26f2e28ae5d
Denis Pankov <idenispankov@gmail.com>
2021-02-25 15:50:46 -0800

    practiced an array methods

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Some data we can work with
-
 const inventors = [
   { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
   { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
@@ -61,23 +60,59 @@ const people = [
 
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
+const bornInFifteenth = inventors.filter(
+  (inventor) => inventor.year >= 1500 && inventor.year < 1600
+);
+console.table(bornInFifteenth);
 
 // Array.prototype.map()
 // 2. Give us an array of the inventors first and last names
+const fullName = inventors.map(
+  (inventor) => `${inventor.first} ${inventor.last}`
+);
+console.table(fullName);
 
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
+const ordered = inventors.sort((a, b) => (a.year > b.year ? 1 : -1));
+console.table(ordered);
 
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live all together?
+const yearsTotalLived = inventors.reduce((total, inventor) => {
+  return total + (inventor.passed - inventor.year);
+}, 0);
+console.log('Years total lived: ');
+console.table(yearsTotalLived);
 
 // 5. Sort the inventors by years lived
+const oldest = inventors.sort((a, b) => {
+  const lastPerson = a.passed - a.year;
+  const nextPerson = b.passed - b.year;
+  return lastPerson > nextPerson ? 1 : -1;
+});
+console.table(oldest);
 
-// 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
-// https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+// // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
+// // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+// const category = document.querySelector('.mw-category');
+
+// // const links = category.querySelectorAll('a');
+// // const de = links.map((link) => link.textContent); - CAN'T DO THAT - IT IS NODE LIST, NOT AN ARRAY - CONVERT TO AN ARRAY
+
+// const links = Array.from(category.querySelectorAll('a'));
+// const de = links
+//   .map((link) => link.textContent)
+//   .filter((streetName) => streetName.includes('de'));
 
 // 7. sort Exercise
 // Sort the people alphabetically by last name
+const alpha = people.sort((lastOne, nextOne) => {
+  const [aLast, aFirst] = lastOne.split(', ');
+  const [bLast, bFirst] = nextOne.split(', ');
+  return aLast > bLast ? 1 : -1;
+});
+console.log(alpha);
 
 // 8. Reduce Exercise
 // Sum up the instances of each of these
@@ -96,4 +131,14 @@ const data = [
   'van',
   'car',
   'truck',
+  'other transports',
 ];
+
+const transportation = data.reduce((obj, item) => {
+  if (!obj[item]) {
+    obj[item] = 0;
+  }
+  obj[item]++;
+  return obj;
+}, {});
+console.log(transportation);
